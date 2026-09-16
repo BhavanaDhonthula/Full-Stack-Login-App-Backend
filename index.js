52911b656f7cef9e13d27fa81a3262d3313f4488
BhavanaDhonthula <225671061+BhavanaDhonthula@users.noreply.github.com>
2026-09-16 13:54:29 +0530

    Commiting full stack login app to git after changing database from sqlite to mongoDb

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-const sqlite3 = require("sqlite3").verbose();
-const { open } = require("sqlite");
 const cors = require("cors");
-const path = require("path");
 const jwtToken = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const User = require("./userSchema");
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 let database;
 
 const initialiseDbAndStartServer = async () => {
-  database = await open({
-    filename: path.join(__dirname, "usersData.db"),
-    driver: sqlite3.Database,
-  });
-
-  console.log(path.join(__dirname, "usersData.db"));
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("Mongo DB Connected successfully");
+    })
+    .catch((err) => {
+      console.log("MongoError:", err);
+    });
 
   app.listen(5000, (err) => {
     if (err) {
       console.log("Error:", err);
     } else {
-      console.log(
-        "Connected to Database and Server running at http://localhost:5000",
-      );
+      console.log("Server running at http://localhost:5000");
     }
   });
 };
@@ -41,20 +44,27 @@ app.post("/register", async (request, response) => {
     response.status(400);
     response.send({ err_msg: "All Fields Are Reuired" });
   } else {
-    const getUserQuery = `select * from user where username='${username}';`;
-    const dbUser = await database.get(getUserQuery);
+    // const getUserQuery = `select * from user where username='${username}';`;
+    const dbUser = await User.findOne({ username: username });
 
-    if (dbUser === undefined) {
-      const hashedPassword = await bcrypt.hash(password, 10);
-      console.log(hashedPassword);
-      const addUserQuery = `INSERT INTO user (username, email, password, mobile_no) values ('${username}', '${email}', '${hashedPassword}', '${mobileNo}');`;
-      await database.run(addUserQuery);
-      response.status(200);
-      response.send({ message: "User Added Successfully" });
-    } else {
+    if (dbUser) {
       response.status(400);
       response.send({ err_msg: "User Already Exist" });
     }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+    // const addUserQuery = `INSERT INTO user (username, email, password, mobile_no) values ('${username}', '${email}', '${hashedPassword}', '${mobileNo}');`;
+    // await database.run(addUserQuery);
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      mobileNo,
+    });
+
+    await newUser.save();
+    response.status(200);
+    response.send({ message: "User Added Successfully" });
   }
 });
 
@@ -62,25 +72,25 @@ app.post("/login", async (request, response) => {
   const { username, password } = request.body;
   console.log(request.body);
 
-  const getDbUser = `select * from user where username = '${username}';`;
+  // const getDbUser = `select * from user where username = '${username}';`;
 
-  const dbUser = await database.get(getDbUser);
+  const dbUser = await User.findOne({ username: username });
 
-  if (dbUser === undefined) {
-    response.status(400);
-    response.send({ err_msg: "Invalid User Credentials" });
-  } else {
+  if (dbUser) {
     const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
     if (isPasswordMatched === true) {
       const jwt_token = jwtToken.sign(
         { username: username },
         "JWT_ACCESS_TOKEN",
       );
-      response.send({ jwt_token });
+      response.send({ jwt_token, username });
     } else {
       response.status(400);
       response.send({ err_msg: "Invalid Username or Password" });
     }
+  } else {
+    response.status(400);
+    response.send({ err_msg: "Invalid User Credentials" });
   }
 });
 
